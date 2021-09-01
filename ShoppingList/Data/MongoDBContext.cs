0915f689708275472e40105af286b311e209ce85
JoaoLopes97 <joaodiogolopes97@hotmail.com>
2021-09-01 13:56:19 +0100

    Add project files.

---
+++ ShoppingList/Data/MongoDBContext.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Data
+{
+    public class MongoDBContext
+    {
+        public static string ConnectionString { get; set; }
+        public static string DatabaseName { get; set; }
+        public static bool IsSSL { get; set; }
+
+        private IMongoDatabase _database { get; }
+
+        public MongoDBContext()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+                if (IsSSL)
+                {
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                }
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(DatabaseName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Can't connect to the server.", ex);
+            }
+        }
+    }
+}
